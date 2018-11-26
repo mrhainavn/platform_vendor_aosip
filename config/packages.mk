d4a0577d0e76e6370604b9a1ea9f7bd8937939f2
mydongistiny <jaysonedson@gmail.com>
2018-11-26 13:42:17 -0800

    prebuilt: Add SubstratumSignature
    Change-Id: Icdefdd2e760d2b95591744f3401b8ad4ebdc9bbf
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: adi8900 <adrianszymanski242@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -17,6 +17,10 @@ PRODUCT_PACKAGES += \
     OmniStyle \
     Eleven
 
+#SubsKey
+PRODUCT_PACKAGES += \
+    SubstratumSignature
+
 # Lawnchair Default Configuration
 PRODUCT_PACKAGES += \
     LawnConf
